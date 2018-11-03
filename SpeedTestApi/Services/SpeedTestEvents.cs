16281bbbf5c02119c5429abd3b2623e70d80d6a1
Per C. Hansen <pch@computas.com>
2018-11-03 17:22:05 +0100

    Lesson4: Added SpeedTestEvents (use of Azure Event Hub)

---
+++ SpeedTestApi/Services/SpeedTestEvents.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using SpeedTestApi.Models;
+
+namespace SpeedTestApi.Services
+{
+    public class SpeedTestEvents : ISpeedTestEvents, IDisposable
+    {
+        private readonly EventHubClient _client;
+
+        public SpeedTestEvents(string connectionString, string entityPath)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(connectionString)
+            {
+                EntityPath = entityPath
+            };
+
+            _client = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+        }
+
+        // Code continues here
+
+        public void Dispose()
+        {
+            _client.CloseAsync();
+        }
+
+        public async Task PublishSpeedTest(TestResult speedTest)
+        {
+            var message = JsonConvert.SerializeObject(speedTest);
+            var data = new EventData(Encoding.UTF8.GetBytes(message));
+
+            await _client.SendAsync(data);
+        }
+    }
+}
